0a1ed7dd353897c36765079c9c94777d0207372c
Robert Carstensen <s504534@mail.nwmissouri.edu>
2018-01-06 19:57:31 -0600

    added tests and methods

--- MobyDickProject/Book.cs
+++ MobyDickProject/Book.cs
@@ -8,7 +8,7 @@ namespace MobyDickProject
 {
     public class Book
     {
-        public List<String> Words { get; set; }
+        public List<String> BookWords { get; set; }
 
         public Book()
         {
@@ -17,20 +17,59 @@ namespace MobyDickProject
 
         public Book(List<String> words)
         {
-            this.Words = words;
+            this.BookWords = words;
+            
         }
 
         public bool containsWords(List<String> searchWords)
         {
+            int count = 0;
+            for(int i = 0; i<searchWords.Count; i++)
+            {
+                count = BookWords.Where(x => x.Equals(searchWords[i])).Count();
 
-            return false;
+                if (count == 0) return false;
+
+            }
+
+
+            return true;
 
         }
 
         public int wordCounts(String countWord)
         {
 
-            return 0;
+            return BookWords.Where(x => x.Equals(countWord)).Count();
+
+        }
+
+
+        public List<String> getDistinctWords()
+        {
+            List<String> returnList = new List<String>();
+
+            foreach(String word in BookWords)
+            {
+                if(returnList.Where(x => x.Equals(word)).Count() == 0)
+                {
+                    returnList.Add(word);
+                }
+            }
+
+            return returnList;
+        }
+
+        public List<int> getWordsCounts(List<String> countWords)
+        {
+            List<int> returnList = new List<int>();
+
+            foreach (String countWord in countWords)
+            {
+                returnList.Add(BookWords.Where(x => x.Equals(countWord)).Count());
+            }
+
+            return returnList;
 
         }
 
--- MobyDickUnitTests/BookTests.cs
+++ MobyDickUnitTests/BookTests.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using MobyDickProject;
+using System.IO;
+using System.Linq;
 
 namespace MobyDickUnitTests
 {
@@ -25,10 +27,6 @@ namespace MobyDickUnitTests
             bool result = myBook.containsWords(checkWords);
 
             Assert.IsTrue(result);
-
-
-
-
             
         }
 
@@ -81,6 +79,72 @@ namespace MobyDickUnitTests
             Book myBook = new Book(bookWords);
 
             bool result = (myBook.wordCounts("Not") == 0);
+
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void getDistinctWordsTest()
+        {
+            bool result = true;
+            List<String> myWordsList = new List<String>();
+            List<String> bookWords = new List<String>();
+            bookWords.Add("Word");
+            bookWords.Add("Hello");
+            bookWords.Add("World");
+            bookWords.Add("Word");
+
+            List<String> correctList = new List<String>();
+            correctList.Add("Word");
+            correctList.Add("Hello");
+            correctList.Add("World");
+
+            Book myBook = new Book(bookWords);
+
+            myWordsList = myBook.getDistinctWords();
+
+            for(int i = 0; i<correctList.Count; i++)
+            {
+                if (!correctList[i].Equals(myWordsList[i]))
+                {
+                    result = false;
+                }
+            }
+            
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void getWordCountsTest()
+        {
+            bool result = true;
+            List<String> myWordsList = new List<String>();
+            List<int> myWordsCountsList = new List<int>();
+            List<String> bookWords = new List<String>();
+            bookWords.Add("Word");
+            bookWords.Add("Hello");
+            bookWords.Add("World");
+            bookWords.Add("Word");
+
+            List<int> correctList = new List<int>();
+            correctList.Add(2);
+            correctList.Add(1);
+            correctList.Add(1);
+
+            Book myBook = new Book(bookWords);
+
+            myWordsList = myBook.getDistinctWords();
+            myWordsCountsList = myBook.getWordsCounts(myWordsList);
+
+            for (int i = 0; i < correctList.Count; i++)
+            {
+                if (!(correctList[i] == myWordsCountsList[i]))
+                {
+                    result = false;
+                }
+            }
 
             Assert.IsTrue(result);
 
